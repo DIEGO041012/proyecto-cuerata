cf05c7d65017d30dfa1b35f87cf476dfdd9d7e46
DIEGO041012 <130582808+DIEGO041012@users.noreply.github.com>
2023-11-29 15:02:53 -0500

    se corrige
    el js de cuerata del carrusel

--- prueba.js
+++ prueba.js
@@ -1,78 +1,25 @@
-// script.js
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-let currentImage = 1;
+const cardImg = document.querySelector(".card img"),
+  menuBtn = document.querySelector(".menu-btn"),
+  popular = document.querySelector(".popular"),
+  more = document.querySelector(".more");
 
-function changeImage() {
-    if (currentImage === 1) {
-        image1.style.opacity = '0';
-        image2.style.opacity = '1';
-        currentImage = 2;
-    } else {
-        image1.style.opacity = '1';
-        image2.style.opacity = '0';
-        currentImage = 1;
-    }
-}
+menuBtn.addEventListener("click", function () {
+  cardImg.classList.toggle("img-expand");
+  menuBtn.firstElementChild.classList.toggle("fa-times");
+  menuBtn.classList.toggle("opened");
+  more.classList.toggle("opened");
 
-// Cambiar la imagen cada 3 segundos (3000 ms)
-setInterval(changeImage, 3000);
+  document.querySelectorAll(".card").forEach((item) => {
+    item.classList.toggle("opened");
+  });
+  document.querySelectorAll(".popular").forEach((item) => {
+    item.classList.remove("opened");
+  });
+});
 
-// Projects section - Left
-  gsap.set(".pbox-left", {
-    opacity: 0,
-    x: -800
+document.querySelectorAll(".popular").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    item.classList.toggle("opened");
   });
-  gsap.to(".pbox-left", {
-    duration: 1.6,
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    ease: "power2.inOut",
-    yoyo: true,
-    scrollTrigger: {
-      trigger: ".projects-section",
-      start: "top center",
-      end: "center",
-      markers: false
-    }
-  });
-  
-  // Projecr section - Right
-  gsap.set(".pbox-right", {
-    x: 500
-  });
-  gsap.to(".pbox-right", {
-    duration: 1.6,
-    x: 0,
-    ease: "power2.inOut",
-    yoyo: true,
-    scrollTrigger: {
-      trigger: ".projects-section",
-      start: "top center",
-      end: "center",
-      marker: false
-    }
-  });
-  
-  // Project section - content section
-  gsap.set(".project-content", {
-    x: -200,
-    y: -200,
-    opacity: 0
-  });
-  gsap.to(".project-content", {
-    duration: 1.6,
-    x: 0,
-    y: 0,
-    opacity: 1,
-    delay: 0.2,
-    ease: "power2.inOut",
-    yoyo: true,
-    scrollTrigger: {
-      trigger: ".projects-section",
-      start: "top center",
-      end: "center",
-      markers: false
-    }
-  });
+});
+
